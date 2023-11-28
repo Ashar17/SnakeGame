dc9637103bbd119211648da1fd704b7d7820a146
jeremy <bluedunkan@gmail.com>
2023-11-27 21:07:32 -0800

    Added new Bomb Apple class that ends the game if you hit it, as well as resource files for it. Then added a parent class for all the apples, then refactored the apples to work with the the parent class. Also pulled from main to make sure everything is up to date.Fixed issue where dependencies weren't pushing.

---
+++ app/src/main/java/com/example/snake/AbstractApple.java
@@ -0,0 +1,63 @@
+package com.example.snake;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+
+import java.util.Random;
+
+abstract class AbstractApple implements IDrawable {
+
+    // The location of the apple on the grid
+    // Not in pixels
+    private Point location = new Point();
+
+    // The range of values we can choose from
+    // to spawn an apple
+    private Point mSpawnRange;
+    private int mSize;
+
+    // An image to represent the apple
+    private Bitmap mBitmapApple;
+
+    // Common constructor for subclasses
+    AbstractApple(Context context, Point sr, int s, int drawableResource){
+        // Make a note of the passed in spawn range
+        mSpawnRange = sr;
+        // Make a note of the size of an apple
+        mSize = s;
+        // Hide the apple off-screen until the game starts
+        location.x = -10;
+
+        // Load the image to the bitmap
+        mBitmapApple = BitmapFactory.decodeResource(context.getResources(), drawableResource);
+
+        // Resize the bitmap
+        mBitmapApple = Bitmap.createScaledBitmap(mBitmapApple, s, s, false);
+    }
+
+    // This is called every time an apple is eaten
+    void spawn(){
+        // Choose two random values and place the apple
+        Random random = new Random();
+        location.x = random.nextInt(mSpawnRange.x) + 1;
+        location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+    }
+
+    // Let SnakeGame know where the apple is
+    // SnakeGame can share this with the snake
+    Point getLocation(){
+        return location;
+    }
+
+    // Draw the apple
+    @Override
+    public void draw(Canvas canvas, Paint paint){
+        canvas.drawBitmap(mBitmapApple,
+                location.x * mSize, location.y * mSize, paint);
+
+    }
+}
